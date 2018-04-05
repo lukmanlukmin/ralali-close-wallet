88768958357f77d29f81e41ddcaa41387b7d9dfd
Anggi Susanto <anggi.susanto@ralali.com>
2018-04-05 14:19:47 +0700

    add transactions table

--- src/migrations/20180405034424-user.js
+++ src/migrations/20180405034424-user.js
@@ -17,11 +17,43 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      createdAt: {
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      currency: {
+        allowNull: false,
+        type: Sequelize.ENUM('IDR','USD','CNY')
+      },
+      user_wallet_recovery1: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      user_wallet_recovery2: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      user_wallet_recovery3: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
